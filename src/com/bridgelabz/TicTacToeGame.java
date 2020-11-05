97989d6ac6240159e40bfea08118b9d1da3b5d5f
“dineshega” <dinesh15.ega@gmail.com>
2020-11-05 13:17:28 +0530

    Added: Displaying the board

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -3,49 +3,65 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class TicTacToeGame {
-	
-		static char[] board;
-		public static void main(String[] args) 
-		{
+
+	static char[] board;
+	public static void main(String[] args) 
+	{
 		System.out.println("Welcome to TicTacToe");
 		board=new char[10];
 		populateBoard();
 		char userInput = chooseXorO();
 		char computer; 
-			if (userInput == 'X')
-			{
-				computer = 'O'; /* computer Input */
-			}
-			else
-			{
-				computer = 'X'; /* computer Input */
-			}
-			System.out.println("userInput " + userInput + " computer entered " + computer);
-		}
-
-		/* usecase2 User input */
-		private static char chooseXorO()
+		if (userInput == 'X')
 		{
-			Scanner Input = new Scanner(System.in);
-			while (true)
-			{	
-				System.out.println("Select X or O");
-				char user = Input.next().toUpperCase().charAt(0);
-				if (user == 'X') {
-					return 'X';
-				} else if (user == 'O') {
-					return 'O';
-				} else {
-					System.out.println("Invalid choice,Enter again");
-				}
+			computer = 'O'; /* computer Input */
+		}
+		else
+		{
+			computer = 'X'; /* computer Input */
+		}
+		System.out.println("userInput " + userInput + " computer entered " + computer);
+		showBoard();
+	}
+	
+	/* usecase3 Displaying the Board */
+	private static void showBoard() 
+	{
+		for (int index = 1; index < board.length; index = index + 3) 
+		{
+			System.out.println("| " + board[index] + " | " + board[index + 1] + " | " + board[index + 2] + " |");
+		}
+	}
+
+	/* usecase2 User input */
+	private static char chooseXorO()
+	{
+		Scanner Input = new Scanner(System.in);
+		while (true)
+		{	
+			System.out.println("Select X or O");
+			char user = Input.next().toUpperCase().charAt(0);
+			if (user == 'X')
+			{
+				return 'X';
+			} 
+			else if (user == 'O') 
+			{
+				return 'O';
+			} 
+			else 
+			{
+				System.out.println("Invalid choice,Enter again");
 			}
 		}
-		
-		/* usecase1 Board is created and assigned empty space to each element*/
-		private static void populateBoard() {
-			for (int index = 1; index < board.length; index++)
-			{
-				board[index] = ' ';
-			}
+	}
+
+	/* usecase1 Board is created and assigned empty space to each element */
+	private static void populateBoard() 
+	{
+		for (int index = 1; index < board.length; index++)
+		{
+			board[index] = ' ';
+		}
 	}
 }
