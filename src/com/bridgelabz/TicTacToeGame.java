376c94636f7ece8fd1d46a221ccde8ace5a3efe8
“dineshega” <dinesh15.ega@gmail.com>
2020-11-05 12:03:56 +0530

    Added: user and computer input

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -5,30 +5,41 @@ import java.util.Scanner;
 public class TicTacToeGame {
 	
 		static char[] board;
+		static char computer;
 		public static void main(String[] args) 
 		{
 		System.out.println("Welcome to TicTacToe");
 		board=new char[10];
 		populateBoard();
 		char userInput= chooseXorO();
-		char Computer;
 			if (userInput == 'X')
 			{
-				Computer = 'O';
+				computer = 'O';
 			} 
 			else
 			{
-				Computer = 'X';
+				computer = 'X';
 			}
+			System.out.println("userInput " + userInput + " computer entered " + computer);
 		}
 
 		/* usecase2 User and Computer input */
 		private static char chooseXorO()
 		{
 			Scanner Input = new Scanner(System.in);
-			System.out.println("Select X or O");
-			char user = Input.next().toUpperCase().charAt(0);
-			return user;
+			while (true)
+			{	
+				System.out.println("Select X or O");
+				char user = Input.next().toUpperCase().charAt(0);
+				if (user == 'X') {
+					return 'X';
+				} else if (user == 'O') {
+					return 'O';
+				} else {
+					System.out.println("Invalid choice,Enter again");
+					continue;
+				}
+			}
 		}
 		
 		/* usecase1 Board is created and assigned empty space to each element*/
