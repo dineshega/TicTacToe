a08a78d5c292d578675d0b0c96329611b3c53d36
“dineshega” <dinesh15.ega@gmail.com>
2020-11-05 10:20:02 +0530

    Welcome to TicTacToe Game

---
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+public class TicTacToeGame {
+		static char[] board;
+		public static void main(String[] args) {
+			board=new char[10];
+		System.out.println("Welcome to TicTacToe");
+		Board();
+	}
+		static void Board() {
+			for (int i = 1; i < 10; i++) {
+				board[i] = ' ';
+			}
+	}
+}
