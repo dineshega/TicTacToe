77dc5629071084728d64f8786a926cc69e16ceaa
“dineshega” <dinesh15.ega@gmail.com>
2020-11-05 10:47:42 +0530

    Added: board is created and assigned empty space to each element

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -3,14 +3,17 @@ package com.bridgelabz;
 public class TicTacToeGame {
 	
 		static char[] board;
-		public static void main(String[] args) {
-			board=new char[10];
+		public static void main(String[] args) 
+		{
 		System.out.println("Welcome to TicTacToe");
-		Board();
-	}
-		static void Board() {
-			for (int i = 1; i < 10; i++) {
-				board[i] = ' ';
+		board=new char[10];
+		populateBoard();
+		}
+
+		static void populateBoard() {
+			for (int index = 1; index < board.length; index++)
+			{
+				board[index] = ' ';
 			}
 	}
 }
