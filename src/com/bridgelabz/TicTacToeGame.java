6925a01fb8f48581f68ef24e8fa669396c5061a0
“dineshega” <dinesh15.ega@gmail.com>
2020-11-05 13:58:03 +0530

    Added: user and computer input

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -3,49 +3,48 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class TicTacToeGame {
-	
-		static char[] board;
-		public static void main(String[] args) 
-		{
+
+	static char[] board;
+	public static void main(String[] args) 
+	{
 		System.out.println("Welcome to TicTacToe");
 		board=new char[10];
-		populateBoard();
-		char userInput = chooseXorO();
-		char computer; 
-			if (userInput == 'X')
-			{
-				computer = 'O'; /* computer Input */
-			}
-			else
-			{
-				computer = 'X'; /* computer Input */
-			}
-			System.out.println("userInput " + userInput + " computer entered " + computer);
-		}
+		createBoard();
+		char userInput=chooseXorO();
+	}
 
-		/* usecase2 User input */
-		private static char chooseXorO()
-		{
-			Scanner Input = new Scanner(System.in);
-			while (true)
-			{	
-				System.out.println("Select X or O");
-				char user = Input.next().toUpperCase().charAt(0);
-				if (user == 'X') {
-					return 'X';
-				} else if (user == 'O') {
-					return 'O';
-				} else {
-					System.out.println("Invalid choice,Enter again");
-				}
-			}
-		}
-		
-		/* usecase1 Board is created and assigned empty space to each element*/
-		private static void populateBoard() {
-			for (int index = 1; index < board.length; index++)
+	/* usecase2 User input */
+	private static char chooseXorO()
+	{
+		char computer='X'; 
+		Scanner Input = new Scanner(System.in);
+		while (true)
+		{	
+			System.out.println("Select X or O");
+			char user = Input.next().toUpperCase().charAt(0);
+			if (user == 'X')
 			{
-				board[index] = ' ';
+				computer='O';
+			} 
+			else if (user == 'O') 
+			{
+				computer='X';
+			} 
+			else 
+			{
+				System.out.println("Invalid choice,Enter again");
 			}
+			System.out.println("userInput " + user + " computer entered " + computer);
+			return user;
+		}
+	}
+
+	/* usecase1 Board is created and assigned empty space to each element */
+	private static void createBoard() 
+	{
+		for (int index = 1; index < board.length; index++)
+		{
+			board[index] = ' ';
+		}
 	}
 }
